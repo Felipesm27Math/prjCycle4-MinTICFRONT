20bba0badfbc69009de5cb0261322d3e142254ad
Felipesm27Math <felipesm27@gmail.com>
2021-12-08 17:36:23 -0500

    front funcionando, solo usar yarn install al descargar y luego yarn start

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+import { UserContext } from 'context/userContext';
+import Index from "pages/Index";
+import IndexUsuarios from "pages/usuarios/Index";
+import  'styles/globals.css';
+import 'styles/tabla.css'
+
+// const httpLink = createHttpLink({
+//   uri:"https://back-nafc-code.herokuapp.com/graphql"
+// })
+
+const client = new ApolloClient({
+  uri:"https://back-nafc-code.herokuapp.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+
+  const [userData, setUserData] = useState({});
+  return (
+    <ApolloProvider client={client}>
+      <UserContext.Provider value={{userData, setUserData}}>
+        <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<Index/>}/>
+                  <Route path="/usuarios" element={<IndexUsuarios/>}/>
+              </Routes>
+          </BrowserRouter>
+      </UserContext.Provider>
+    </ApolloProvider>
+  );
+}
+
+export default App;
