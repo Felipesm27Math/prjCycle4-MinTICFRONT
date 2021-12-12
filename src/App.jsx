e29438c4f733047b293ba94b4eb169c6c6d0e0c8
Felipesm27Math <felipesm27@gmail.com>
2021-12-12 13:57:04 -0500

    Modificaciones a APP.jsx para tenerlo funcional e igual al que esta en features

--- src/App.jsx
+++ src/App.jsx
@@ -12,63 +12,13 @@ import IniciarSesion from 'pages/auth/login';
 import  'styles/globals.css';
 import 'styles/tabla.css';
 
-
-import jwt_decode from 'jwt-decode';
-import 'styles/globals.css';
-import 'styles/tabla.css';
-
-
-
-
-const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
-});
-
-const authLink = setContext((_, { headers }) => {
-  
-  const token = JSON.parse(localStorage.getItem('token'));
-  
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
 const client = new ApolloClient({
   uri:"http://localhost:4000/graphql",
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
 });
 
 function App() {
   const [userData, setUserData] = useState({});
-  const [authToken, setAuthToken] = useState('');
-
-  const setToken = (token) => {
-    console.log('set token', token);
-    setAuthToken(token);
-    if (token) {
-      localStorage.setItem('token', JSON.stringify(token));
-    } else {
-      localStorage.removeItem('token');
-    }
-  };
-
-  useEffect(() => {
-    if (authToken) {
-      const decoded = jwt_decode(authToken);
-      setUserData({
-        _id: decoded._id,
-        nombre: decoded.nombre,
-        identificacion: decoded.identificacion,
-        correo: decoded.correo,
-        rol: decoded.rol,
-      });
-    }
-  }, [authToken]);
-
   return (
     <ApolloProvider client={client}>
       <UserContext.Provider value={{userData, setUserData}}>
@@ -92,4 +42,3 @@ function App() {
 
 
 export default App;
-
